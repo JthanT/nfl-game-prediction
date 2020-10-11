cb8bce0d0edfb5f6050a11e09aee2d3ccd2e900c
Johnathan Tam <johnathantam@Johnathans-MacBook-Pro.local>
2020-10-10 18:24:10 -0600

    Updated current game week and added dedicated prediction accuracy icons to lists

--- src/pages/PastSeasonGameList.tsx
+++ src/pages/PastSeasonGameList.tsx
@@ -14,6 +14,8 @@ import { GAME_SCHEDULE_BY_YEAR_QUERY } from '../graphql/queries/game.queries';
 import { timeSelections, currentLeagueTimes } from '../utils/time';
 import DialogBox from '../components/DialogBox';
 import PageLoading from '../components/PageLoading';
+import RightPredictionIcon from '../components/icons/RightPredictionIcon';
+import WrongPredictionIcon from '../components/icons/WrongPredictionIcon';
 
 const useStyles = makeStyles({
     rightRoot: {
@@ -37,6 +39,11 @@ const useStyles = makeStyles({
         display: 'flex',
         width: '100px',
         paddingRight: '30px',
+    },
+    winnerRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
     },
 });
 
@@ -139,18 +146,13 @@ function PastSeasonGameList() {
                                     customBodyRender: (value, tableMeta) => {
                                         const accuracy = predictionCorrectness(tableMeta.rowData[3], tableMeta.rowData[4]);
                                         return (
-                                            <Typography
-                                                className={
-                                                    accuracy === 'right' ? 
-                                                    classes.rightRoot : (
-                                                        accuracy === 'wrong' ? 
-                                                        classes.wrongRoot : 
-                                                        classes.undeterminedRoot
-                                                    )
-                                                }
-                                            >
-                                                {value}
-                                            </Typography>
+                                            <div className={classes.winnerRow}>
+                                                <Typography>
+                                                    {value}
+                                                </Typography>
+                                                {accuracy === 'right' && <RightPredictionIcon />}
+                                                {accuracy === 'wrong' && <WrongPredictionIcon />}
+                                            </div>
                                         );
                                     }
                                 }
